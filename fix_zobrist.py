c76facd4674a994ffa049bd5e6fb5b7bf0e7915f
Supratsa <supraontwitch708@gmail.com>
2025-08-18 22:28:58 +0530

    Create fix_zobrist.py

---
+++ fix_zobrist.py
@@ -0,0 +1,6 @@
+import fileinput
+
+file_path = "./engines/supraengine"
+
+for line in fileinput.input(file_path, inplace=True):
+    print(line.replace("zobrist_hash()", "transposition_key()"), end='')
